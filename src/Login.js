4509500813ce268f23dccc6624e7538a56da8c70
Nikhil <nvverma1998@gmail.com>
2023-04-10 23:07:54 +0530

    Initial commit

---
+++ src/Login.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Button } from "@mui/material";
+import "./Login.css";
+import { auth, provider } from "./firebase";
+import {actionTypes} from "./reducer";
+import {useStateValue} from "./StateProvider";
+
+
+
+function Login() {
+    const [{}, dispatch] = useStateValue();
+
+
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        dispatch({
+            type: actionTypes.SET_USER,
+            user: result.user,
+        });
+
+      })
+      .catch((error) => alert(error.message));
+  };
+  return (
+    <div className="login">
+      <h1></h1>
+      <div className="login__container">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1024px-WhatsApp.svg.png"
+          alt=""
+        />
+
+        <div className="login__text">
+          <h1>sign in to whatsapp</h1>
+        </div>
+
+        <Button onClick={signIn}>Sign In With Google</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
+ 
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPQC6Idhi1mLioHzA_6vgFuTwYRjQzgqc",
+  authDomain: "new-clone-ab04f.firebaseapp.com",
+  projectId: "new-clone-ab04f",
+  storageBucket: "new-clone-ab04f.appspot.com",
+  messagingSenderId: "335226502805",
+  appId: "1:335226502805:web:8204f39d83f9f8e449e59c"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {auth,provider};
+export default db;
+
